62aabd3ebc47b417923192c9086b30bfe3204173
hs6860 <jang_6860@sookmyung.ac.kr>
2020-11-23 22:23:47 +0900

    2

--- main.c
+++ main.c
@@ -5,34 +5,34 @@
 
 
 #define N 6
+
 int gameboard(int a[N][N])
 {
 	int i,j;
 	int *ptr;
 	
 	ptr = &a[0][0];
-	
-	printf("    0    1    2    3    4    5    ");
+	printf("     0    1    2    3    4    5    \n");
 	for(i=0;i<6;i++)
 	{
-		printf("+----+----+----+----+----+----+\n");
-		printf(" %c |", i+'a');
+		printf("   +----+----+----+----+----+----+\n");
+		printf(" %c |", i+'a'); //보드의 세로줄 번호  
 		
 		for(j=0;j<6;j++)
 		{
-			unsigned char k;
-			if(k==1)
-				printf("●|"); 
-			else if(k==2)
-				printf("○|");
-			else if(k==0)				// empty : 0 , black : 1, white : 2
-				printf("X |"); 
+		
+			if(ptr==1)
+				printf("  1 |"); 
+			else if(ptr==2)
+				printf("  ○|");
+			else if(ptr==0)				// empty : 0 , black : 1, white : 2
+				printf("  X |"); 
 			else
-				printf("  |");
+				printf("    |");
 		}
-		purtchar('\n');
+		printf("\n");
 	}
-	printf("+----+----+----+----+----+----+\n");
+	printf("   +----+----+----+----+----+----+\n");
 }
 
 
@@ -40,7 +40,7 @@ int gameboard(int a[N][N])
 
 void main(int argc, char *argv[]) {
 	
-	int gameboardinitboard[N][N] = //게임초기화   
+	int initboard[N][N] =    
 	{
 		
 		{3, 3, 3, 3, 3, 3},     
@@ -48,21 +48,12 @@ void main(int argc, char *argv[]) {
 		{3, 3, 1, 2, 3, 3},	
 		{3, 3, 2, 1, 3, 3},	
 		{3, 3, 3, 3, 3, 3},
-		{3, 3, 3, 3, 3, 3},	
+		{3, 3, 3, 3, 3, 3}
 		
 			
 	};
 	
-	
-void putboard(gameboard, int put, int turn)
-{
-	int p,t;
-	int ant_i = (turn^0*3);
-	int dxy[N] = {}
-		
-}	
-	
-
+	gameboard(initboard);
 	
 
 	return 0;
